9e301c7503a923f92031df993b2c5fde0446dc65
Ian Noyes <ian@12southmusic.com>
2014-03-26 12:26:05 -0500

    close comment on index.php

--- index.php
+++ index.php
@@ -16,4 +16,4 @@ define('WP_USE_THEMES', true);
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
 
-/** Add extra line back to git
+/** Add extra line back to git*/
